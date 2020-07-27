227a7b628c1b4312f4e1bf5afebe7599f87a65e2
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2020-07-27 14:50:56 -0400

    Add navbar

--- shared.js
+++ shared.js
@@ -14,7 +14,18 @@ for (const button of selectPlanButtons) {
 function hideModal() {
     backdrop.style.display = 'none';
     modal.style.display = 'none';
+    mobileNav.style.display = 'none';
 }
 
 backdrop.addEventListener("click", hideModal);
 noModal.addEventListener("click", hideModal);
+
+
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+toggleButton.addEventListener("click", function () {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+
+});
